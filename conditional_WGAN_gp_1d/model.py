47abd7ac82250a9af3bdef2af06a7cd6f43223ae
First User <ldap_cheyu@rex.ym.edu.tw>
2021-01-02 17:25:01 +0800

    model modify and training process

--- conditional_WGAN_gp_1d/model.py
+++ conditional_WGAN_gp_1d/model.py
@@ -2,50 +2,63 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 import tensorflow.keras as keras
 from tensorflow.keras import layers
-import os, sys
+import os
+import sys
+
 
 class Generator(keras.Model):
-    def __init__ (self):
-        super(Generator,self).__init__()
+    def __init__(self):
+        super(Generator, self).__init__()
         self.fc_layer_z1 = layers.Dense(128, use_bias=False, activation=tf.nn.leaky_relu)
-        self.fc_layer_y1 = layers.Dense(128,use_bias=False, activation=tf.nn.leaky_relu)
-#         self.bn_1 = layers.BatchNormalization()
 
-        self.fc_layer_2 = layers.Dense(256, use_bias=False, activation=tf.nn.leaky_relu)
-#         self.bn_2 = layers.BatchNormalization()
+        self.fc_layer_y1 = layers.Dense(128, use_bias=False, activation=tf.nn.leaky_relu)
 
-        self.fc_layer_3 = layers.Dense(512, use_bias=False, activation= tf.nn.leaky_relu)
-#         self.bn_3 = layers.BatchNormalization()
+        self.combine_dense = layers.Dense(256, use_bias=False, activation=tf.nn.leaky_relu)
 
-        self.output_layer = tf.keras.layers.Dense(73, use_bias=False, activation='sigmoid')
+        self.fc_layer_2 = layers.Dense(512, use_bias=False, activation=tf.nn.leaky_relu)
 
-    def call(self, inputs, labels training = None):
-        x = self.fc_layer_1(inputs)
-        
-        x = self.fc_layer_2(x)
-        x = self.fc_layer_3(x)
+        self.output_layer = tf.keras.layers.Dense(72, use_bias=False, activation='sigmoid')
+
+    def call(self, inputs, labels, training=False):
+        z = self.fc_layer_z1(inputs)
+        y = self.fc_layer_y1(labels)
+
+        # combine_dense
+        combined_x = self.combine_dense(tf.concat([z, y], axis=-1))
+        x = self.fc_layer_2(combined_x)
         x = self.output_layer(x)
-        
+
         return x
-        
+
+
 class Discriminator(keras.Model):
     def __init__(self):
         super(Discriminator, self).__init__()
         self.fc_layer_1 = layers.Dense(512, use_bias=False, activation=tf.nn.leaky_relu)
-#         self.bn_1 = layers.BatchNormalization()
 
         self.fc_layer_2 = layers.Dense(256, use_bias=False, activation=tf.nn.leaky_relu)
-#         self.bn_2 = layers.BatchNormalization()
 
         self.fc_layer_3 = layers.Dense(128, use_bias=False, activation=tf.nn.leaky_relu)
 
-#         self.bn_3 = layers.BatchNormalization()
-        self.output_layer = layers.Dense(1, use_bias=False, activation='sigmoid')
+        self.fc_layer_label = layers.Dense(128, use_bias=False, activation=tf.nn.leaky_relu)
 
-    def call(self, inputs, labels training = None):
+        self.valid_layer = layers.Dense(1, use_bias=False, activation='sigmoid')
+
+        self.output_labels = layers.Dense(1, use_bias=False, activation='sigmoid')
+
+    def call(self, inputs, training=False):
         x = self.fc_layer_1(inputs)
         x = self.fc_layer_2(x)
         x = self.fc_layer_3(x)
-        x = self.output_layer(x)
 
-        return x
+        # Discriminator
+        result = self.valid_layer(x)
+
+        if training:
+            # Recognition
+            label = self.fc_layer_label(x)
+            label = self.output_labels(label)
+
+            return result, label
+        else:
+            return result
